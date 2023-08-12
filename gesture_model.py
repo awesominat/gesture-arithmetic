0542b1aa5297425de1ce9cb5762bd98521b4af09
awesominat <110934811+awesominat@users.noreply.github.com>
2023-08-13 03:57:06 +0400

    added training, experimented with model

--- gesture_model.py
+++ gesture_model.py
@@ -1,34 +1,39 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from math import sqrt
 
 class GestureModel(nn.Module):
     def __init__(self, classes):
         super().__init__()
-        self.linear1 = nn.Linear(126, 3000).to(torch.float64)
-        self.norm1 = nn.BatchNorm1d(3000)
-        self.dropout1 = nn.Dropout(.8)
+        self.linear1 = nn.Linear(42, 128)
+        self.norm1 = nn.BatchNorm1d(128)
+        self.dropout1 = nn.Dropout(.3)
+        torch.nn.init.kaiming_normal_(self.linear1.weight, sqrt(5))
 
-        self.linear2 = nn.Linear(3000, 2500)
-        self.norm2 = nn.BatchNorm1d(2500)
-        self.dropout2 = nn.Dropout(.8)
+        self.linear2 = nn.Linear(128, 256)
+        self.norm2 = nn.BatchNorm1d(256)
+        self.dropout2 = nn.Dropout(.3)
+        torch.nn.init.kaiming_uniform_(self.linear2.weight)
 
-        self.linear3 = nn.Linear(2500, 2000)
-        self.norm3 = nn.BatchNorm1d(2000)
-        self.dropout3 = nn.Dropout(.8)
+        self.linear3 = nn.Linear(256, 256)
+        self.norm3 = nn.BatchNorm1d(256)
+        self.dropout3 = nn.Dropout(.3)
+        torch.nn.init.kaiming_uniform_(self.linear3.weight)
 
-        self.linear4 = nn.Linear(2000, 1000)
-        self.norm4 = nn.BatchNorm1d(1000)
-        self.dropout4 = nn.Dropout(.8)
+        self.linear4 = nn.Linear(256, 512)
+        self.norm4 = nn.BatchNorm1d(512)
+        self.dropout4 = nn.Dropout(.3)
+        torch.nn.init.kaiming_normal_(self.linear4.weight, sqrt(5))
 
-        self.linear5 = nn.Linear(1000, classes)
+        self.linear5 = nn.Linear(512, classes)
+        torch.nn.init.kaiming_normal_(self.linear5.weight, sqrt(5))
 
     def forward(self, x):
-        x = self.dropout1(self.norm1(F.relu(self.linear1(x))))
-        x = self.dropout2(self.norm2(F.relu(self.linear2(x))))
-        x = self.dropout3(self.norm3(F.relu(self.linear3(x))))
-        x = self.dropout4(self.norm4(F.relu(self.linear4(x))))
+        x = self.dropout1(F.elu(self.norm1(self.linear1(x))))
+        x = self.dropout2(F.elu(self.norm2(self.linear2(x))))
+        x = self.dropout3(F.elu(self.norm3(self.linear3(x))))
+        x = self.dropout4(F.elu(self.norm4(self.linear4(x))))
 
         x = self.linear5(x)
         # F.log_softmax(x, dim = 1)
--- image_dataset_loader.py
+++ image_dataset_loader.py
@@ -2,9 +2,10 @@ import mediapipe as mp
 import numpy as np
 import os
 import random
+import torch
 import glob
 from torch.utils.data import Dataset, DataLoader, random_split
-from torchvision.transforms import ToTensor
+# from torchvision.transforms import ToTensor/
 from PIL import Image
 
 
@@ -72,32 +73,47 @@ def process_image(image_path, label, ISTRAIN=False):
                 break
     
     concat_displays = ''
-    hand_world_landmarks_tuples = np.zeros((2, 21, 3)) # left/right hand, 21 land marks, (x,y,z) pos
+    hand_world_landmarks_tuples = np.zeros((1, 21, 2)) # left/right hand, 21 land marks, (x,y,z) pos
 
     running_ind = 0
     for handed in detection_result.handedness:
         display_name = handed[0].display_name
-        if display_name == 'Left':
-            ind = 0
-        elif display_name == 'Right':
-            ind = 1
+        # if display_name == 'Left':
+        ind = 0
+        # elif display_name == 'Right':
+        #     ind = 1
         if concat_displays == '':
             concat_displays += display_name
         else:
             concat_displays += ', ' + display_name
-        hand_world_landmarks_tuples[ind, :] = [(obj.x, obj.y, obj.z) for obj in detection_result.hand_world_landmarks[running_ind]]
+        hand_world_landmarks_tuples[ind, :] = [(obj.x, obj.y) for obj in detection_result.hand_world_landmarks[running_ind]]
+        # hand_world_landmarks_tuples[ind, :] = [(obj.x, obj.y, obj.z) for obj in detection_result.hand_world_landmarks[running_ind]]
         running_ind += 1
 
     # since it is not guaranteed for a left/right hand to be in the image, the np array is already padded
     # which hopefully silences the layers using the left/right hand
 
     # flatten the array to pass into the nn
-    flattened = hand_world_landmarks_tuples.reshape((1, 126))
+    # flattened = hand_world_landmarks_tuples.reshape((126, 1))
+    # flattened = torch.flatten(torch.from_numpy(hand_world_landmarks_tuples))
 
-    if len(detection_result.handedness) != expected_len:
-        print('discarding ' + image_path)
-        return np.zeros((1, 126))
-    return flattened
+    # if len(detection_result.handedness) != expected_len:
+    #     print('discarding ' + image_path)
+    #     return np.zeros((1, 84))
+    return hand_world_landmarks_tuples
+
+def augment_landmarks(landmarks, flip_horizontal_prob=0.5, flip_vertical_prob=0.5, jitter_std=0.002):
+    if random.random() < flip_horizontal_prob:
+        landmarks[:, :, 0] = -landmarks[:, :, 0]
+
+    if random.random() < flip_vertical_prob:
+        landmarks[:, :, 1] = -landmarks[:, :, 1]
+
+    non_zero_mask = landmarks != 0
+    noise = torch.randn_like(landmarks) * jitter_std
+    landmarks += noise * non_zero_mask.float()
+
+    return landmarks
 
 class CustomImageDataset(Dataset):
     image_paths: str
@@ -106,6 +122,7 @@ class CustomImageDataset(Dataset):
     def __init__(self, image_paths, labels, transform=None):
         self.image_paths = image_paths
         self.labels = labels
+        self.transform = transform
 
     def __len__(self):
         return len(self.image_paths)
@@ -113,11 +130,16 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.image_paths[idx]
         label = self.labels[idx]
+        landmarks = torch.from_numpy(process_image(img_path, label, True))
 
+        # if self.transform:
+        #     landmarks = augment_landmarks(landmarks)
 
-        return img_path, label
+        flattened = torch.flatten(landmarks).to(torch.float32).to('cuda')
 
-def create_datasets(root_dir, val_samples_per_class=3, test_samples_per_class=3):
+        return flattened, label
+
+def create_datasets(root_dir, val_samples_per_class=3, test_samples_per_class=3, transform=None):
     all_image_paths = glob.glob(os.path.join(root_dir, '*.jpg'))
     label_to_paths = {}
     
@@ -125,7 +147,7 @@ def create_datasets(root_dir, val_samples_per_class=3, test_samples_per_class=3)
         label = int(os.path.basename(path).split()[0])
         if label not in label_to_paths:
             label_to_paths[label] = []
-        to_append = process_image(path, label, True)
+        # to_append = process_image(path, label, True)
         label_to_paths[label].append(path) # to_append
 
     train_paths, val_paths, test_paths = [], [], []
@@ -139,8 +161,8 @@ def create_datasets(root_dir, val_samples_per_class=3, test_samples_per_class=3)
     val_labels = [int(os.path.basename(path).split()[0]) for path in val_paths]
     test_labels = [int(os.path.basename(path).split()[0]) for path in test_paths]
 
-    train_dataset = CustomImageDataset(train_paths, train_labels)
-    val_dataset = CustomImageDataset(val_paths, val_labels)
-    test_dataset = CustomImageDataset(test_paths, test_labels)
+    train_dataset = CustomImageDataset(train_paths, train_labels, transform)
+    val_dataset = CustomImageDataset(val_paths, val_labels, transform)
+    test_dataset = CustomImageDataset(test_paths, test_labels, transform)
 
     return train_dataset, val_dataset, test_dataset
